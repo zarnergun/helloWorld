50e26db02e50f6a549d7bc8c242078c03fb5c0da
zam <deceirem@gmail.com>
2020-08-24 22:54:31 +0200

    ALGO FONCTIONNEL ; Premiere lettre non prise en charge ;

--- src/Correcteur.java
+++ src/Correcteur.java
@@ -3,10 +3,7 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.*;
 
 class checkMotExiste implements Callable<Boolean> {
@@ -190,10 +187,10 @@ class Correcteur extends JFrame {
 
     JPanel container = new JPanel();
     JTextArea fieldText = new JTextArea();
+    JTextArea correctField = new JTextArea();
     Map<Character, ArrayList<String>> listeDesMots = new HashMap<>();
     ExecutorService executorService = new ThreadPoolExecutor(1, 1, 1000,
             TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-
 
     public Correcteur() throws ExecutionException, InterruptedException {
         this.setTitle("Correcteur");
@@ -209,14 +206,53 @@ class Correcteur extends JFrame {
         this.setVisible(true);
     }
 
+    static int calculLevenshtien(String x, String y) {
+        int[][]dp = new int[x.length() + 1][y.length() + 1];
+
+        for (int i = 0; i <= x.length(); i++) {
+            for (int j = 0; j <= y.length(); j++) {
+                if (i == 0) {
+                    dp[i][j]= j;
+                }
+                else if (j == 0) {
+                    dp[i][j]= i;
+                }
+                else {
+                    dp[i][j]= min(dp[i - 1][j - 1]
+                                    + coupDeChangement(x.charAt(i - 1), y.charAt(j - 1)),
+                            dp[i - 1][j]+ 1,
+                            dp[i][j - 1]+ 1);
+                }
+            }
+        }
+
+        return dp[x.length()][y.length()];
+    }
+
+    public static int coupDeChangement(char a, char b) {
+        return a == b ? 0 : 1;
+    }
+
+    public static int min(int... numbers) {
+        return Arrays.stream(numbers)
+                .min().orElse(Integer.MAX_VALUE);
+    }
+
     private void initIHM() {
-        fieldText.setPreferredSize(new Dimension(400, 300));
+        fieldText.setPreferredSize(new Dimension(400, 150));
+        correctField.setPreferredSize(new Dimension(400, 150));
+        correctField.setBackground(Color.LIGHT_GRAY);
+        correctField.setEditable(false);
+        correctField.setText("Corrections possibles :");
         fieldText.setBorder(BorderFactory.createRaisedBevelBorder());
+        container.add(fieldText);
+        container.add(correctField);
         fieldText.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
+                String textUpdate = fieldText.getText().toLowerCase();
                 try {
-                    if (!fieldText.getText().isEmpty()) {
+                    if (!textUpdate.isEmpty() && textUpdate.length() >= 2) {
                         warn();
                     }
                 } catch (ExecutionException | InterruptedException executionException) {
@@ -226,9 +262,10 @@ class Correcteur extends JFrame {
 
             @Override
             public void removeUpdate(DocumentEvent e) {
+                String textUpdate = fieldText.getText().toLowerCase();
                 try {
-                    System.out.println("texte : " + fieldText.getText());
-                    if (!fieldText.getText().isEmpty()) {
+                    System.out.println("texte : " + textUpdate);
+                    if (!textUpdate.isEmpty() && textUpdate.length() >= 2) {
                         warn();
                     }
                 } catch (ExecutionException | InterruptedException executionException) {
@@ -238,8 +275,9 @@ class Correcteur extends JFrame {
 
             @Override
             public void changedUpdate(DocumentEvent e) {
+                String textUpdate = fieldText.getText().toLowerCase();
                 try {
-                    if (!fieldText.getText().isEmpty()) {
+                    if (!textUpdate.isEmpty() && textUpdate.length() >= 2) {
                         warn();
                     }
                 } catch (ExecutionException | InterruptedException executionException) {
@@ -248,18 +286,36 @@ class Correcteur extends JFrame {
             }
 
             public void warn() throws ExecutionException, InterruptedException {
-                checkMotExiste check = new checkMotExiste(fieldText.getText(), listeDesMots);
+                checkMotExiste check = new checkMotExiste(fieldText.getText().toLowerCase(), listeDesMots);
                 Future<Boolean> future = executorService.submit(check);
+                TreeMap<Integer, String> listeCorrection = new TreeMap<Integer, String>();
                 boolean motExist = future.get();
                 System.out.println(fieldText.getText() + "->" + motExist);
                 if (motExist) {
                     fieldText.setForeground(Color.blue);
+                    correctField.setText("Mot correcte");
                 } else {
                     fieldText.setForeground(Color.red);
+                    correctField.setText("Corrections possibles : ");
+                    Character firstLetter = fieldText.getText().toLowerCase().charAt(0);
+                    for(String s : listeDesMots.get(firstLetter) ) {
+                        int retour = calculLevenshtien(fieldText.getText().toLowerCase(), s);
+                        if(retour <= (fieldText.getText().length()) )  {
+                            listeCorrection.put(retour, s);
+                        }
+                    }
+                    int compteur = 0;
+                    for (Map.Entry<Integer, String> mapData : listeCorrection.entrySet() ) {
+                        if(compteur<5) {
+                            System.out.println("Key : " + mapData.getKey() + "Value : " + mapData.getValue());
+                            compteur = mapData.getKey();
+                            correctField.setText(correctField.getText() + "\n " + mapData.getValue());
+                        }
+                    }
+                    //System.out.println("Trier :" + triAvecValeur(listeCorrection).entrySet());
                 }
 
             }
         });
-        container.add(fieldText);
     }
 }
